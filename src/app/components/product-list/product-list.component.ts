4a54a4e07ad36bcd902fbd9cef0dfa6b661c6224
Nurmukhammad Begiboev <nurmukhammad.begiboev@capgemini.com>
2021-05-06 20:24:02 +0200

    Seach field completed

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
 
   constructor(private productService: ProductService, 
               private route: ActivatedRoute ) { }
@@ -22,17 +23,37 @@ export class ProductListComponent implements OnInit {
     });
   }
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+      if(this.searchMode){
+        this.handleSearchProducts();
+      }
+      else{
+
+    this.handleListProducts();
+          }
+  }
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+    this.productService.searchProducts(theKeyword).subscribe(
+        data=> {
+          this.products = data;
+        }
+    );
+  }
+
+  private handleListProducts() {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
-    if(hasCategoryId){
+    if (hasCategoryId) {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
     }
-    else{
+    else {
       this.currentCategoryId = 1;
     }
-   this.productService.getProductList(this.currentCategoryId).subscribe(
-     data => {
-       this.products = data;
-     }
-   )
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
   }
 }
