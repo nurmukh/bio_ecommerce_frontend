fcecc5845abf56efa49441357cb7683be25dfd14
Nurmukhammad Begiboev <nurmukhammad.begiboev@capgemini.com>
2021-05-06 08:20:03 +0200

    initial Search button added

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ import { Product } from '../common/product';
 export class ProductService {
 
   private baseUrl = "http://localhost:8080/api/products"
+  private categoryUrl = "http://localhost:8080/api/product-category"
+
 
   constructor(private httpClient: HttpClient) { }
  
@@ -17,15 +20,28 @@ export class ProductService {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );    
+     
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );  
   }
   
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
-    products: Product[]
+    products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
